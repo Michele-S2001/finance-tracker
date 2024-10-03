7c97c9ff11e782128f602b9dd0f6cfd8fbe4fb55
Michele-S2001 <serafini.michele01@gmail.com>
2024-10-03 17:12:12 +0200

    Added - index page, default layout with an appHeader component

---
+++ app/components/AppHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <header class="flex justify-between items-center py-6">
+    <NuxtLink to="/" class="text-xl font-bold">Finance tracker</NuxtLink>
+    <div>
+      <UAvatar
+        src="https://avatars.githubusercontent.com/u/739984?v=4"
+        alt="Avatar"
+      />
+    </div>
+  </header>
+</template>
