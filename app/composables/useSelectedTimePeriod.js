0061e5b0037361b851dc3beaeabbe7d6acc4e332
Michele-S2001 <serafini.michele01@gmail.com>
2024-10-07 17:00:12 +0200

    Refactor - useSelectedTimePeriod now returns the current and previous time periods as two separate computed values

--- app/composables/useSelectedTimePeriod.js
+++ app/composables/useSelectedTimePeriod.js
@@ -1,47 +1,52 @@
 import { startOfDay, startOfMonth, startOfYear, endOfMonth, endOfYear, endOfDay, sub } from 'date-fns'
 
 export const useSelectedTimePeriod = (period) => {
-  const dates = computed(
+  const current = computed(
     () => {
       switch (period.value) {
         case 'Yearly':
           return {
-            current: {
-              from: startOfYear(new Date()),
-              to: new Date()
-            },
-            previous: {
-              from: startOfYear(sub(new Date(), { years: 1})),
-              to: endOfYear(sub(new Date(), { years: 1}))
-            }
+            from: startOfYear(new Date()),
+            to: new Date()
           }
         case 'Monthly':
           return {
-            current: {
-              from: startOfMonth(new Date()),
-              to: new Date()
-            },
-            previous: {
-              from: startOfMonth(sub(new Date(), { month: 1})),
-              to: endOfMonth(sub(new Date(), { month: 1}))
-            }
+            from: startOfMonth(new Date()),
+            to: new Date()
           }
         case 'Daily':
           return {
-            current: {
-              from: startOfDay(new Date()),
-              to: new Date()
-            },
-            previous: {
-              from: startOfDay(sub(new Date(), { day: 1})),
-              to: endOfDay(sub(new Date(), { day: 1}))
-            }
+            from: startOfDay(new Date()),
+            to: new Date()
           }
-        default:
-          break;
       }
     }
   )
 
-  return dates
+  const previous = computed(
+    () => {
+      switch (period.value) {
+        case 'Yearly':
+          return {
+            from: startOfYear(sub(new Date(), { years: 1})),
+            to: endOfYear(sub(new Date(), { years: 1}))
+          }
+        case 'Monthly':
+          return {
+            from: startOfMonth(sub(new Date(), { month: 1})),
+            to: endOfMonth(sub(new Date(), { month: 1}))
+          }
+        case 'Daily':
+          return {
+            from: startOfDay(sub(new Date(), { day: 1})),
+            to: endOfDay(sub(new Date(), { day: 1}))
+          }
+      }
+    }
+  )
+
+  return {
+    current,
+    previous
+  }
 }
