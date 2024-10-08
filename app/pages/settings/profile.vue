00e924267338cb074a6f99730dfae3c8010b1ded
Michele-S2001 <serafini.michele01@gmail.com>
2024-10-08 15:23:12 +0200

    Added - saving user metadata & changing email into settings > profile form

--- app/pages/settings/profile.vue
+++ app/pages/settings/profile.vue
@@ -1,5 +1,5 @@
 <template>
-  <UForm :state="state" :schema="schema">
+  <UForm :state="state" :schema="schema" @submit="saveProfile">
     <UFormGroup class="mb-4" label="Full Name" name="name">
       <UInput v-model="state.name" />
     </UFormGroup>
@@ -8,7 +8,7 @@
       <UInput v-model="state.email" />
     </UFormGroup>
 
-    <UButton type="submit" color="black" variant="solid" label="Save" :pending="pending" /> 
+    <UButton type="submit" color="black" variant="solid" label="Save" :laoding="pending" :disabled="pending"/> 
   </UForm>
 </template>
 
@@ -20,9 +20,40 @@ const { toastSuccess, toastError } = useAppToast()
 
 const pending = ref(false)
 const state = ref({
-  name: '',
+  name: user.value.user_metadata?.full_name,
   email: user.value.email
 })
+
+const saveProfile = async () => {
+  pending.value = true
+  try {
+    const data =  {
+      data: {
+        full_name: state.value.name
+      }
+    }
+
+    if(state.value.email !== user.value.email) {
+      data.email = state.value.email
+    }
+
+    const { error } = await supabase.auth.updateUser(data)
+    if (error) throw error
+
+    toastSuccess({
+      title: 'Profile updated',
+      description: 'Yuor profile has been updated'
+    })
+  } catch(e) {
+    console.error(e.message)
+    toastError({
+      title: 'Error updating profile',
+      description: e.message
+    })
+  } finally {
+    pending.value = false
+  }
+}
 
 const schema = z.object({
   name: z.string().min(2).optional(),
