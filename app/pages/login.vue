77556d79c6366a70fcbe5ecdfacd76744d2cb999
Michele-S2001 <serafini.michele01@gmail.com>
2024-10-08 09:28:07 +0200

    Added - a static login page

---
+++ app/pages/login.vue
@@ -0,0 +1,33 @@
+<template>
+  <UCard v-if="!success">
+    <template #header>
+      Sign-in to Finance Tracker
+    </template>
+
+    <form>
+      <UFormGroup label="Email" name="email" class="mb-4" :required="true" help="You'll receive an email with the confirmation link">
+        <UInput type="email" placeholder="Email" required />
+      </UFormGroup>
+      <!-- ! Il click è da togliere -->
+      <UButton type="submit" variant="solid" color="black" @click="success = true">Sign-in</UButton>
+    </form>
+  </UCard>
+
+  <UCard v-else>
+    <template #header>
+      Email has been sent
+    </template>
+
+    <div class="text-center">
+      <p>We have sent an email to <strong>lorem@hotmail.it</strong> with a link to sign-in</p>
+      <p>
+        <strong>Important: </strong>The link will expire in 5 minutes !
+      </p>
+    </div>
+  </UCard>
+
+</template>
+
+<script setup>
+const success = ref(false)
+</script>
