4b4f1b19419ff755c2a2bfeb7d64016e8c368991
Michele-S2001 <serafini.michele01@gmail.com>
2024-10-09 10:53:44 +0200

    Added - dinamic url for deploying

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,5 +11,11 @@ export default defineNuxtConfig({
 
   supabase: {
     redirect: true
+  },
+
+  runtimeConfig: {
+    public: {
+      baseUrl: process.env.BASE_URL ?? "http://localhost:3000"
+    }
   }
 })
